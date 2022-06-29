8dcb33ea99fa4e56fbc81a64673d7f79fda367ed
Britt Carlton <brittanyrc95@gmail.com>
2022-06-29 13:53:19 -0400

    Add APIs, library for fetching/working with weather and astronomy data

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,9 @@
 // global variables
-
+// API keys/urls
+const weatherApiKey = "57bb6de1daa898857366ae01e5539fb5";
+// where the iss at? does not require a key
+// base url is to get information about iss at a given time
+const issApiBaseUrl = "https://api.wheretheiss.at/v1/satellites/25544/positions?timestamps=";
 
 // functions
 
