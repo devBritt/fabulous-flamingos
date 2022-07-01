0178575c58c5d2455533459e1decb4b5a6f62898
Britt Carlton <brittanyrc95@gmail.com>
2022-06-30 20:18:59 -0400

    Build out html

--- assets/js/script.js
+++ assets/js/script.js
@@ -6,7 +6,15 @@ const weatherApiKey = "57bb6de1daa898857366ae01e5539fb5";
 const issApiBaseUrl = "https://api.wheretheiss.at/v1/satellites/25544/positions?timestamps=";
 
 // functions
+// function to retrieve date/time
+function getDateTime() {
+    // TODO: make sure to use today's date to set min and value attribute in html dynamically
+}
 
+// function to dynamically create forecast cards
+function createForecastCards() {
+    // TODO: on page load, create cards using today's date as first card
+}
 
 // event handlers
 
